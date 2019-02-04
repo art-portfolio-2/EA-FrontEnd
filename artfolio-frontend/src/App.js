a31ce44d0fe2b4e41daf9e6c6a8682b3da033458
siratl <elishubax@gmail.com>
2019-02-04 11:50:30 -0800

    Created components and routes for various pages in app

--- artfolio-frontend/src/App.js
+++ artfolio-frontend/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import { Route } from 'react-router-dom'
+import AboutMe from './components/AboutMe'
+import Home from './components/Home'
+import Contact from './components/Contact'
+import Portfolio from './components/Portfolio'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Artfolio 2</h1>
+      <NavBar/>
+      <Route exact path='/' component ={Home} />
+      <Route path='/about' component ={AboutMe} />
+      <Route path='/portfolio' component ={Portfolio} />
+      <Route path='/contact' component ={Contact} />
+      
       </div>
     );
   }
---
+++ artfolio-frontend/src/components/AboutMe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+
+const AboutMe = () => {
+  return (
+    <div className="aboutPage">
+      <Jumbotron fluid>
+        <div className="jumbo">
+          <h1>About Me</h1>
+          <p className="lead">- Client Name</p>
+        </div>
+      </Jumbotron>
+      <section className="mainContent" />
+    </div>
+  );
+};
+
+export default AboutMe;
